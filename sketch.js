45ad5ca29d7cffca8eead5ea8286b81b4cb62895
Murilo Barbosa <mbn2@cin.ufpe.br>
2023-02-25 16:19:15 -0300

    Padronize identation across files
    Co-authored-by: RenatoNogFer <RenatoNogFer@users.noreply.github.com>
    Co-authored-by: Frederico Janovitz <fjanovitz@users.noreply.github.com>

--- sketch.js
+++ sketch.js
@@ -1,19 +1,19 @@
 let grid;
 
 function setup() {
-  let numRows = 25;
-  let numCols = 25;
-  let cellSize = 20;
-  let terrainColors = [
-    [69, 69, 69], // Fucking void
-    [120, 180, 110], // Forest
-    [250, 227, 173],   // Desert
-    [170, 220, 223]   // Ocean
-  ];
+    let numRows = 25;
+    let numCols = 25;
+    let cellSize = 20;
+    let terrainColors = [
+        [69, 69, 69], // Fucking void
+        [120, 180, 110], // Forest
+        [250, 227, 173],   // Desert
+        [170, 220, 223]   // Ocean
+    ];
 
-  grid = new Grid(numRows, numCols, cellSize, terrainColors);
+    grid = new Grid(numRows, numCols, cellSize, terrainColors);
 }
 
 function draw() {
-  grid.display();
+    grid.display();
 }
