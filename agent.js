576fd567df4c57ad94934b354951b226753c4d1e
Murilo Barbosa <mbn2@cin.ufpe.br>
2023-02-25 18:13:22 -0300

    Add move function to agent
    Co-authored-by: RenatoNogFer <RenatoNogFer@users.noreply.github.com>
    Co-authored-by: Frederico Janovitz <fjanovitz@users.noreply.github.com>
    Co-authored-by: Leônidas Netto <ldcnetto@users.noreply.github.com>

--- agent.js
+++ agent.js
@@ -5,6 +5,14 @@ class Agent {
         this.r = 6;
     }
 
+    move(){
+        let target = createVector(this.moves[0][0], this.moves[0][1]);
+        let dist = p5.Vector.dist(target, this.pos);
+        this.vel.set(p5.Vector.sub(target, this.pos));
+        this.vel.limit(this.maxVel);
+
+        if(dist < this.r/2 && this.moves.length > 1) this.moves.splice(0, 1);
+    }
 
     display() {
         push();
