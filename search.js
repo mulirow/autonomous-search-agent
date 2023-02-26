6960ae4e3477da5b4d6164147ddf7d8addc66fb6
RenatoNogFer <rnfo@cin.ufpe.br>
2023-02-26 18:22:04 -0300

    Updated search direction order
    Co-authored-by: Leônidas Netto <ldcnetto@users.noreply.github.com>
    Co-authored-by: Frederico Janovitz <fjanovitz@users.noreply.github.com>
    Co-authored-by: Murilo Barbosa <mulirow@users.noreply.github.com>

--- search.js
+++ search.js
@@ -14,6 +14,9 @@ class Search {
         if (col < this.matrix[0].length - 1 && this.matrix[row][col + 1] != -1) {
             neighbors.push([row, col + 1]);
         }
+        if (row < this.matrix.length - 1 && this.matrix[row + 1][col] != -1) {
+            neighbors.push([row + 1, col]);
+        }
         if (col > 0 && this.matrix[row][col - 1] != -1) {
             neighbors.push([row, col - 1]);
         }
