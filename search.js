b32f2fe45f2f8181da443f3fb7f274d8dd34607e
RenatoNogFer <rnfo@cin.ufpe.br>
2023-02-26 18:30:26 -0300

    Fix search positions
    Co-authored-by: Murilo Barbosa <Mulirow@users.noreply.github.com>
    Co-authored-by: Frederico Janovitz <fjanovitz@users.noreply.github.com>
    Co-authored-by: Leônidas Netto <ldcnetto@users.noreply.github.com>
    Co-authored-by: Victoria Luquet <vicluq@users.nopreply.github.com>

--- search.js
+++ search.js
@@ -21,9 +21,25 @@ class Search {
         if (col > 0 && this.matrix[row][col - 1] != -1) {
             neighbors.push([row, col - 1]);
         }
-        if (row < this.matrix.length - 1 && this.matrix[row + 1][col] != -1) {
-            neighbors.push([row + 1, col]);
-        }
         return neighbors;
+    }
+
+    display(){
+        push();
+        noStroke();
+        fill(200, 0, 200);
+        rectMode(RADIUS);
+        for(let i = 0; i < this.movesMade.length; i++) {
+            for(let j = 0; j < neighborsList[i].length; j++) {
+                rect(neighborsList[i][j][0] * cellSize + cellSize / 2, neighborsList[i][j][1] * cellSize + cellSize / 2, cellSize / 8, cellSize / 8);
+            }
+        }
+        stroke(255);
+        strokeWeight(2);
+        fill(180, 0, 128);
+        for(let i = 0; i < this.movesMade.length; i++) {
+            rect(this.movesMade[i][0], this.movesMade[i][1], this.r, this.r);
+        }
+        pop();
     }
 }
