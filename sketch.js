6ed752fa7a0868ea9f8ea0bce8db47045ad935b4
vicluq <victoria.tewari@gmail.com>
2023-02-27 20:45:28 -0300

    added graphic interface to change search method

--- sketch.js
+++ sketch.js
@@ -1,25 +1,41 @@
 let grid;
 let agent;
 let search_alg;
-let opt_alg;
 let drawState = 0;
+let selectionMode = 1;
+let selectedAlgorithm = 2;
+let numRows = 25;
+let numCols = 25;
+let cellSize = 20;
+let terrainColors = [
+  [69, 69, 69], // Fucking void
+  [120, 180, 110], // Forest
+  [250, 227, 173], // Desert
+  [170, 220, 223], // Ocean
+];
+let costs = [-1, 1, 3, 9];
+
+let algorithmList = [
+  AStar,
+  UniformCostSearch,
+  DepthFirstSearch,
+  BreadthFirstSearch,
+  GreedySearch,
+];
 
 function setup() {
-    let numRows = 25;
-    let numCols = 25;
-    let cellSize = 20;
-    let terrainColors = [
-        [69, 69, 69], // Fucking void
-        [120, 180, 110], // Forest
-        [250, 227, 173],   // Desert
-        [170, 220, 223]   // Ocean
-    ];
-    let costs = [-1, 1, 3, 9];
+  grid = new Grid(numRows, numCols, cellSize, terrainColors, costs);
+  agent = new Agent(
+    grid.startPoint[0] * cellSize + cellSize / 2,
+    grid.startPoint[1] * cellSize + cellSize / 2
+  );
+}
 
-    grid = new Grid(numRows, numCols, cellSize, terrainColors, costs);
-    agent = new Agent(grid.startPoint[0]*cellSize + cellSize/2, grid.startPoint[1]*cellSize + cellSize/2);
+function draw() {
+  if (selectionMode) {
+    setup();
     // Algorithm picked by the user
-    search_alg = new AStar(grid.terrainCosts); 
+    search_alg = new algorithmList[selectedAlgorithm](grid.terrainCosts);
 
     // Algorithm that runs in the background to show the optimal path
     opt_alg = new UniformCostSearch(grid.terrainCosts);
@@ -27,27 +43,38 @@ function setup() {
     search_alg.setPath(grid.startPoint, grid.endPoint);
     opt_alg.setPath(grid.startPoint, grid.endPoint);
     agent.coordToPos(search_alg.agentPath, cellSize);
-}
 
-function draw() {
-    grid.display();
-    if(drawState >= 0) drawState += search_alg.display(grid.cellSize);
-    if(drawState >= 1) drawState += opt_alg.displayOptPath(grid.cellSize);
-    if(drawState >= 2) drawState += agent.run();
+    selectionMode = 0;
+  }
 
-    // Reset the search mechanisms and defines a new food location
-    if(p5.Vector.dist(agent.pos, createVector(grid.endPoint[0] * grid.cellSize + grid.cellSize / 2, grid.endPoint[1] * grid.cellSize + grid.cellSize / 2)) == 0) {
-        grid.startPoint = [...grid.endPoint];
-        let endRow, endCol;
-        do {
-            endRow = floor(random(grid.numRows));
-            endCol = floor(random(grid.numCols));
-        } while (grid.terrainColors[grid.terrain[endRow][endCol]] == grid.terrainColors[0]);
-        grid.endPoint = [endRow, endCol];
+  grid.display();
+  if (drawState >= 0) drawState += search_alg.display(grid.cellSize);
+  if (drawState >= 1) drawState += opt_alg.displayOptPath(grid.cellSize);
+  if (drawState >= 2) drawState += agent.run();
 
-        drawState = 0;
-        search_alg.setPath(grid.startPoint, grid.endPoint);
-        opt_alg.setPath(grid.startPoint, grid.endPoint);
-        agent.coordToPos(search_alg.agentPath, grid.cellSize);
-    }
+  // Reset the search mechanisms and defines a new food location
+  if (
+    p5.Vector.dist(
+      agent.pos,
+      createVector(
+        grid.endPoint[0] * grid.cellSize + grid.cellSize / 2,
+        grid.endPoint[1] * grid.cellSize + grid.cellSize / 2
+      )
+    ) == 0
+  ) {
+    grid.startPoint = [...grid.endPoint];
+    let endRow, endCol;
+    do {
+      endRow = floor(random(grid.numRows));
+      endCol = floor(random(grid.numCols));
+    } while (
+      grid.terrainColors[grid.terrain[endRow][endCol]] == grid.terrainColors[0]
+    );
+    grid.endPoint = [endRow, endCol];
+
+    drawState = 0;
+    search_alg.setPath(grid.startPoint, grid.endPoint);
+    opt_alg.setPath(grid.startPoint, grid.endPoint);
+    agent.coordToPos(search_alg.agentPath, grid.cellSize);
+  }
 }
