31e72b37d9f690962e92faaa69aaad30712756ea
Murilo Barbosa <mbn2@cin.ufpe.br>
2023-02-26 21:39:34 -0300

    Fix DFS to return correct agent path

--- dfs.js
+++ dfs.js
@@ -1,6 +1,7 @@
 class DepthFirstSearch extends Search {
     constructor(terrainCosts) {
         super(terrainCosts);
+        this.parents = [];
     }
 
     dfs(row, col, target, searchPath) {
@@ -17,8 +18,9 @@ class DepthFirstSearch extends Search {
         for (let i = 0; i < neighbors.length; i++) {
             let neighbor = neighbors[i];
             if (!this.visited[neighbor[0]][neighbor[1]]) {
+                this.parents[neighbor[0]][neighbor[1]] = [row, col];
                 searchPath = this.dfs(neighbor[0], neighbor[1], target, searchPath);
-                
+
                 if (searchPath[searchPath.length-1][0] == target[0] && searchPath[searchPath.length-1][1] == target[1]) {
                     return searchPath;
                 }
@@ -31,14 +33,28 @@ class DepthFirstSearch extends Search {
     setPath(startPoint, endPoint) {
         for (let i = 0; i < this.matrix.length; i++) {
             this.visited[i] = [];
+            this.parents[i] = [];
             for (let j = 0; j < this.matrix[i].length; j++) {
                 this.visited[i][j] = false;
+                this.parents[i][j] = null;
             }
         }
 
+        this.movesMade = 0;
+        this.agentPath = [];
         this.searchPath = [];
         this.neighborsList = [];
+
         this.searchPath = this.dfs(startPoint[0], startPoint[1], endPoint, []);
-        this.agentPath = [...this.searchPath];
+
+        let optimalPath = [];
+        let current = [endPoint[0], endPoint[1], this.matrix[endPoint[0]][endPoint[1]]];
+        optimalPath.push(current);
+        while(current[0] !== startPoint[0] || current[1] !== startPoint[1]) {
+            current = this.parents[current[0]][current[1]];
+            optimalPath.unshift([current[0], current[1], this.matrix[current[0]][current[1]]]);
+        }
+
+        this.agentPath = optimalPath;
     }
 }
