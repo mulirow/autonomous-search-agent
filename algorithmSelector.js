b8b89986b1a2de1d4d732c40b69e9249bb916f64
Murilo Barbosa <mbn2@cin.ufpe.br>
2023-02-28 00:36:07 -0300

    Fix indentation

--- algorithmSelector.js
+++ algorithmSelector.js
@@ -1,20 +1,20 @@
 const buttonList = document.getElementsByClassName("nav_button");
 
 function selectAlgorithm(alg, button) {
-  selectedAlgorithm = alg;
-  selectionMode = 1;
+    selectedAlgorithm = alg;
+    selectionMode = 1;
 
-  buttonList.forEach((button, index) => {
-    if (selectedAlgorithm === index) {
-      button.classList.add("selected");
-    } else {
-      button.classList.remove("selected");
-    }
-  });
+    buttonList.forEach((button, index) => {
+        if (selectedAlgorithm === index) {
+            button.classList.add("selected");
+        } else {
+            button.classList.remove("selected");
+        }
+    });
 }
 
 buttonList.forEach((button, index) => {
-  button.addEventListener("click", () => {
-    selectAlgorithm(index, button);
-  });
+    button.addEventListener("click", () => {
+        selectAlgorithm(index, button);
+    });
 });
